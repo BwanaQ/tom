20acb985edbfb76ba78aca1997fe807328e63cfb
BwanaQ <thunjawax@gmail.com>
2024-04-04 07:47:56 +0300

    add mobile menu

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -5,7 +5,7 @@ import heart from '../../assets/heart.png'
 const Footer = () => {
   return (
     <footer className="footer">
-        &copy; 2024. Built with lots of<img src={heart} alt="love" className="heart" />by <a href="/" target="_self" rel="noopener noreferrer">Tom Hunja</a>
+        &copy; 2024. Built with lots of<img src={heart} alt="love" className="heart" />by <a href="/" target="_self" rel="noopener noreferrer">Tom Hunja</a>.
     </footer>
   );
 }
--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import './navbar.css'
+import React, { useState } from "react";import './navbar.css'
 import logo from '../../assets/logo.png'
 import contactImg from '../../assets/contact.png'
+import hamburger from '../../assets/hamburger.png'
 import {Link} from 'react-scroll';
 
 
 const Navbar = () => {
+  const [showMenu, setshowMenu] = useState(false);
   return (
     <nav className='navbar'>
         <img src={logo} alt='Logo' className='logo'/>
@@ -21,6 +22,16 @@ const Navbar = () => {
           <img src={contactImg} alt="text Bubble" className="desktopMenuImg" />
           Contact Me
         </button>
+        
+        
+        <img src={hamburger} alt='Menu' className='mobMenu' onClick={()=>setshowMenu(!showMenu)}/>
+        <div className="navMenu" style={{display: showMenu? 'flex':'none'}}>
+          <Link activeClass='active' to='hero' spy={true} smooth={true} offset={-100} duration={500} className="ListItem" onClick={()=>setshowMenu(false)}>Home</Link>
+          <Link activeClass='active' to='skills' spy={true} smooth={true} offset={-50} duration={500} className="ListItem" onClick={()=>setshowMenu(false)}>About</Link>
+          <Link activeClass='active' to='works' spy={true} smooth={true} offset={-50} duration={500} className="ListItem" onClick={()=>setshowMenu(false)}>Portfolio</Link>
+          <Link activeClass='active' to='contactPage' spy={true} smooth={true} offset={-50} duration={500} className="ListItem" onClick={()=>setshowMenu(false)}>Clients</Link>
+          <Link activeClass='active' to='contact' spy={true} smooth={true} offset={-50} duration={500} className="ListItem" onClick={()=>setshowMenu(false)}>Contact</Link>
+        </div> 
     </nav>
   )
 }
