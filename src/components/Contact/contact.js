1fe5383e645e4da64c010acdeff16aa3f641eecb
BwanaQ <thunjawax@gmail.com>
2024-04-05 09:11:26 +0300

    modify recaptcha to v3

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -1,46 +1,59 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect } from 'react';
 import emailjs from '@emailjs/browser';
-import ReCAPTCHA from 'react-google-recaptcha';
+
 import './contact.css';
-import client1 from '../../assets/client-1.png';
-import client2 from '../../assets/client-2.webp';
-import client3 from '../../assets/client-3.png';
-import linkedIn from '../../assets/linkedin.png';
-import github from '../../assets/github.png';
+import client1 from'../../assets/client-1.png';
+import client2 from'../../assets/client-2.webp';
+import client3 from'../../assets/client-3.png';
+import linkedIn from'../../assets/linkedin.png';
+import github from'../../assets/github.png';
 
 const Contact = () => {
   const form = useRef();
-  const recaptchaRef = useRef();
   const siteKey = process.env.REACT_APP_RECAPTCHA_SITE_KEY;
-  const sendEmail = (e) => {
+  useEffect(() => {
+    const loadReCaptchaScript = () => {
+      const script = document.createElement('script');
+      script.src = `https://www.google.com/recaptcha/api.js?render=${siteKey}`;
+      script.async = true;
+      document.body.appendChild(script);
+    };
+
+    loadReCaptchaScript();
+  }, []);
+
+  const sendEmail = async (e) => {
     e.preventDefault();
 
-    // Verify reCAPTCHA response
-    recaptchaRef.current.executeAsync().then((token) => {
-      emailjs
-        .sendForm('tom_hunja_portfolio', 'template_v8ztf4e', form.current, {
-          publicKey: 'fDqtspqunmXi2ivGX',
-          'g-recaptcha-response': token, // Add reCAPTCHA token to the request
-        })
-        .then(
-          () => {
-            console.log('SUCCESS!');
-            alert('Your Email has been Sent.');
-            e.target.reset();
-          },
-          (error) => {
-            console.log('FAILED...', error.text);
-          }
-        );
-    });
+    // Execute reCAPTCHA v3 to get a token
+    const token = await window.grecaptcha.execute('YOUR_SITE_KEY', { action: 'submit' }); // Replace YOUR_SITE_KEY with your actual reCAPTCHA v3 site key
+
+    // Include the obtained token in the form data
+    const formData = new FormData(form.current);
+    formData.append('token', token);
+
+    // Send the form data with the token to your backend for verification
+    emailjs
+      .sendForm('tom_hunja_portfolio', 'template_v8ztf4e', formData, {
+        publicKey: 'fDqtspqunmXi2ivGX',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+          alert("Your Email has been Sent.");
+          form.current.reset();
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
   };
 
   return (
     <section id="contactPage">
       <div id="clients">
         <span className="contactPageTitle">Happy Clients</span>
-        <br />
-        <br />
+        <br /><br />
         <span className="clientDesc">Some of the notable clients I have had the opportunity to work with include</span>
         <div className="clientImgs">
           <img src={client1} alt="Picha safari" className="clientImg" />
@@ -50,19 +63,12 @@ const Contact = () => {
       </div>
       <div id="contact">
         <span className="contactPageTitle">Contact Me</span>
-        <br />
-        <br />
+        <br /><br />
         <span className="contactDesc">Feel free to reach out to me directly for collaborations, inquiries, or just to say hello!</span>
         <form ref={form} className="contactForm" onSubmit={sendEmail}>
           <input type="text" className="name" placeholder="Your Name" name="from_name" />
           <input type="email" className="email" placeholder="Your Email" name="from_email" />
           <textarea name="message" className="msg" placeholder="Your Message" rows="5"></textarea>
-          <ReCAPTCHA
-            ref={recaptchaRef}
-            sitekey = {siteKey}
-            size="invisible"
-            badge="bottomright"
-          />
           <button type="submit" value="send" className="submitBtn">Submit</button>
           <div className="links">
             <a href="https://www.linkedin.com/in/tom-hunja/" target="_blank" rel="noopener noreferrer"><img src={linkedIn} alt="LinkedIn" className="link" /></a>
